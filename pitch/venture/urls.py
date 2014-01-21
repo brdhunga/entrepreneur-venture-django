342333708c1ca45c950c5682074a1f28217f8a3c
Bivu Raj Dhungana <brdhunga@gmail.com>
2014-01-21 01:19:00 -0500

    Added ajax method to send venture applications

--- pitch/venture/urls.py
+++ pitch/venture/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from venture.views import VentureListView, VentureCreateView, VentureUpdateView, VentureApplicationView, all_ventures
+from venture.views import VentureListView, VentureCreateView, VentureUpdateView,\
+                            VentureApplicationView, all_ventures, apply_ventures
+
 
 
 urlpatterns = patterns('',
@@ -9,7 +11,8 @@ urlpatterns = patterns('',
     url(r'^ventures/$', VentureListView.as_view(), name='venture_list'),
     url(r'^ventures/edit/(?P<id>[\w-]+)$', VentureUpdateView.as_view(), name = 'venture_edit'),
     url(r'^ventures/application/$', VentureApplicationView.as_view(), name = 'venture_application'),
-    url(r'^all_ventures/$', all_ventures, name='all_ventures'),    
+    url(r'^all_ventures/$', all_ventures, name='all_ventures'),
+    url(r'^apply/', apply_ventures , name = 'apply_ventures'),
 )
 
 #url( r'^blogs/(?P<post_slug>[-a-zA-Z0-9]+)/?$', 'blog.views.single_blog', name = 'single_blog'),
--- pitch/venture/views.py
+++ pitch/venture/views.py
@@ -6,7 +6,11 @@ from venture.forms import VentureForm
 from venture.models import Venture
 
 
+
 class VentureListView(ListView):
+    '''
+    outputs list of all ventures to entrepreneurs
+    '''
     model = Venture
     template_name = 'venture/ventures_all.html'
 
@@ -17,7 +21,12 @@ class VentureListView(ListView):
         context = super(VentureListView, self).get_context_data(**kwargs)
         return context
 
+
+
 class VentureCreateView(CreateView):
+    '''
+    creates new venture for entrepreneurs
+    '''
     form_class = VentureForm
     model = Venture
     template_name = 'venture/ventures_add.html'
@@ -28,7 +37,12 @@ class VentureCreateView(CreateView):
         return super(VentureCreateView, self).form_valid(form)
 
 
+
+
 class VentureUpdateView(UpdateView):
+    '''
+    updates a specific venture
+    '''
     form_class = VentureForm
     model = Venture
     template_name = 'venture/ventures_edit.html'
@@ -39,7 +53,12 @@ class VentureUpdateView(UpdateView):
         return obj
 
 
-class VentureApplicationView(ListView):
+
+
+class VentureApplicationView(VentureListView):
+    '''
+    lists all the ventures
+    '''
     model = Venture
     template_name = 'venture/ventures_applications.html'
 
@@ -56,7 +75,21 @@ class VentureApplicationView(ListView):
 @csrf_exempt
 def all_ventures(request):
     user = request.user
-    the_json = serializers.serialize('json', Venture.objects.filter(user = user), indent=2, use_natural_keys=True)
-    print "all the ventures are"
+    the_json = serializers.serialize('json',
+                Venture.objects.filter(user = user),
+                indent=2, use_natural_keys=True)
+    return HttpResponse(the_json)
+
+
+
+
+@csrf_exempt
+def apply_ventures(request):
+    user = request.user
+    mentor_name = request.POST['mentor_name']
+    mentor_username = request.POST['mentor_username']
+    chosen_venture = request.POST['chosen_venture']
+    print "the response"
+    the_json = str(mentor_name)+" "+str(mentor_username)+" "+str(chosen_venture)+" "+str(user)
     print the_json
     return HttpResponse(the_json)
