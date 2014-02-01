16c41f41d6c85fdd52f13811b58ddf5a8f51fe05
Bivu Raj <brdhunga@gmail.com>
2014-02-01 06:18:20 -0500

    unused apps deleted

--- pitch/profiles/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from profiles.models import ProfileDetail
-
-class ProfileDetailAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(ProfileDetail, ProfileDetailAdmin)
--- pitch/profiles/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from profiles.models import ProfileDetail
-
-class ProfileDetailForm(ModelForm):
-	class Meta:
-		model = ProfileDetail
-		fields = ['dob', 'state_of_residency', 'city_of_residency', 'state_of_office', 'city_of_office']
